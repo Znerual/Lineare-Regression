29f938d0fff0ba7f8009fe25175b61c4858a541c
Laurenz <laurenz_ruzicka@gmx.at>
2020-04-20 19:29:09 +0200

    Final

--- Lineare Regression.f90
+++ Lineare Regression.f90
@@ -15,30 +15,74 @@
     program LineareRegression
 
     implicit none
-
+    double precision a1,b1, a2,b2
     ! Variables
-    double precision :: sx= 0d0, sy = 0d0, sxx = 0d0, sxy = 0d0, a, b
-    integer :: s = 0, stat
-    double precision :: x,y
-    open(1, file="lin-reg.txt", action="read")
-    do  
-        read(1, *, iostat=stat) x,y
-        if (stat /= 0) exit !fall es einen Fehler gab oder wir am Ende sind beende die Schleife
-        s = s + 1
-        sx = sx + x
-        sxx = sxx + x**2
-        sy = sy + y
-        sxy = sxy + y * x   
-    end do
-    
-    close(1)
-    
-    a = (sxx * sy - sx*sxy)/(s * sxx - sx**2)
-    b = (s*sxy - sx*sy)/(s*sxx - sx**2)
-    open(2, file="fit-parameter.dat", action="write")
-    write(2,*) "Fit für f(x) = a + b x mit a = " , a, " und b = " ,b
-    close(2)
+    call fitLinReg(a1,b1)
+    call fitBrown(a2,b2)
+   
+    open(3, file="fit-parameter.dat", action="write")
+    write(3,*) "Fit für f(x) = a + b x"
+    write(3,*) "Lin-reg.txt"
+    write(3,*) "a = " , a1, " und b = " ,b1
+    write(3,*) "random_walk2.dat"
+    write(3,*) "a = " , a2, " und b = " ,b2
+
+    close(3)
 
 
     end program LineareRegression
 
+    subroutine fitLinReg(a,b)
+        implicit none
+        double precision, intent(out) ::a, b
+        double precision :: sx, sy, sxx, sxy
+        integer :: s,stat
+        double precision :: x,y    
+        open(1, file="lin-reg.txt", action="read")
+        s = 0
+        sx = 0d0
+        sy = 0d0
+        sxx = 0d0
+        sxy = 0d0
+        do
+            read(1, *, iostat=stat) x,y
+            if (stat /= 0) exit
+            s = s + 1
+            sx = sx + x
+            sxx = sxx + x**2
+            sy = sy + y
+            sxy = sxy + y * x
+        end do
+        
+        close(1)
+        a = (sxx * sy - sx*sxy)/(s * sxx - sx**2)
+        b = (s*sxy - sx*sy)/(s*sxx - sx**2)
+    end subroutine fitLinReg
+    subroutine fitBrown(a,b)
+        implicit none
+        double precision, intent(out) ::a, b
+        double precision :: sx, sy, sxx, sxy
+        integer :: s,stat
+        double precision :: x,y    
+        
+        open(2, file="random_walk2d.dat", action="read")
+        s = 0
+        sx = 0d0
+        sy = 0d0
+        sxx = 0d0
+        sxy = 0d0
+        do
+            read(2, *, iostat=stat) x,y
+            if (stat /= 0) exit
+            y = y**2 !um die Eingabe zu linearisieren
+            s = s + 1
+            sx = sx + x
+            sxx = sxx + x**2
+            sy = sy + y
+            sxy = sxy + y * x
+        end do
+        
+        close(2)
+        a = (sxx * sy - sx*sxy)/(s * sxx - sx**2)
+        b = (s*sxy - sx*sy)/(s*sxx - sx**2)
+    end subroutine fitBrown
--- plot.gp
+++ plot.gp
@@ -4,7 +4,11 @@ set output "Lineare Regression.pdf"
 f(x) = a + b * x
 fit f(x) "lin-reg.txt"  via a,b
 
-plot "lin-reg.txt" using 1:2 title "Data", f(x) title "Fit"
+fb(x) = c + d * x
+fit fb(x) "random_walk2d.dat" using 1:($2*$2) via c,d
+
+plot "lin-reg.txt" using 1:2 title "Lin Reg", f(x) title "LR - Fit"
+plot "random_walk2d.dat" using 1:($2*$2) title "Random Walk", fb(x) title "RW - Fit"
 
 unset term
 unset output
